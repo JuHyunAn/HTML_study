01d470aa94b20323e231f7095ca64220eb71e817
JuHyunAn <dkswngus7@gmail.com>
2019-07-07 01:07:51 +0900

    javascript

---
+++ code16/js/main.js
@@ -0,0 +1,27 @@
+// x를 변수로 선언(값이 정해지지 않은 상태)> undefinded.
+var x;
+console.log(x);
+
+// x 값을 10으로 할당
+x = 10
+console.log(x);
+
+// 두가지 값을 동시에 나타낼땐 ,(콤마) 사용으로 구분
+var y = 10, z = 20
+console.log(y, z);
+
+// Number(숫자형)
+console.log(typeof x);
+
+// String(문자형), 
+var str = "문자형";
+console.log(str);
+
+// Boolean(논리형) > true(참), false(거짓)을 구분
+console.log(typeof true, typeof false);
+
+// null
+
+// 변수가 갖는 자료의 형태는 총 5가지 (undefinded/숫자형/문자형/논리형/null)
+// console.log()를 통해 개발자 도구 콘솔에서 값을 확인할 수 있다.
+// typeof 를 통해 변수의 자료 형태를 파악할 수 있다.
