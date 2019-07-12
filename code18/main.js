357b8c3d62334ffe21616f5badf1abf30620df6d
JuHyunAn <dkswngus7@gmail.com>
2019-07-13 00:43:56 +0900

    javascript

---
+++ code18/main.js
@@ -0,0 +1,7 @@
+var str3 = 1 + 1 + "안녕";  // string : 2안녕
+console.log(typeof str3, str3);
+
+var str4 = 1 + 1;           // number : 2
+console.log(typeof str4, str4);
+
+// 자바스크립트 언어는 문자열과 숫자를 결합할 경우 '문자열 데이터 타입'이 됩니다.
